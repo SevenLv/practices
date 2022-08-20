9723225694e5bd2f3af01dd3b2e8a3b09f4b293f
Seven Lv <lvhongqiang@msn.cn>
2022-08-20 20:35:43 +0800

    update test_boolean_and.c

--- tests/test_boolean_and.c
+++ tests/test_boolean_and.c
@@ -7,6 +7,13 @@ int main(void)
     const int err_none = 0x0;
     const int err_error1 = 0x1;
     const int err_error2 = 0x2;
+    const int err_error3 = 0x4;
+    const int err_error4 = 0x8;
+    const int err_error5 = 0x10;
+    const int err_error6 = 0x20;
+    const int err_error7 = 0x40;
+    const int err_error8 = 0x80;
+    const int err_error9 = 0x100;
 
     int result = err_none;
 
@@ -28,7 +35,68 @@ int main(void)
         result |= err_error2;
     }
 
-    // TODO:
+    expected = FALSE;
+    value1 = TRUE;
+    value2 = BOOLEAN_UNKNOWN;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error3;
+    }
+
+    expected = FALSE;
+    value1 = FALSE;
+    value2 = TRUE;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error4;
+    }
+
+    expected = FALSE;
+    value1 = FALSE;
+    value2 = FALSE;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error5;
+    }
+
+    expected = FALSE;
+    value1 = FALSE;
+    value2 = BOOLEAN_UNKNOWN;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error6;
+    }
+
+    expected = FALSE;
+    value1 = BOOLEAN_UNKNOWN;
+    value2 = TRUE;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error7;
+    }
+
+    expected = FALSE;
+    value1 = BOOLEAN_UNKNOWN;
+    value2 = FALSE;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error8;
+    }
+
+    expected = FALSE;
+    value1 = BOOLEAN_UNKNOWN;
+    value2 = BOOLEAN_UNKNOWN;
+
+    if (expected != BOOLEAN_AND(value1, value2))
+    {
+        result |= err_error9;
+    }
 
     return result;
 }
