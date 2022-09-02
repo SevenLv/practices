516ae922dbd92fd8625d1dd5115a129e7e676d03
吕洪强 <lvhongqiang@msn.cn>
2022-09-02 10:23:10 +0800

    update crc8 test

---
+++ tests/test_crc8.h
@@ -0,0 +1,24 @@
+#ifndef INC_TEST_CRC8_H_
+#define INC_TEST_CRC8_H_
+
+/* includes */
+#include "pra_boolean.h"
+#include "pra_crc8.h"
+
+/* macros */
+
+/* types */
+
+typedef pra_boolean (*get_func)(pra_crc8 *const, uint32_t *const);
+
+/* variables */
+
+/* functions */
+
+int test_get(get_func get);
+
+int test_init(get_func get);
+
+int test_compute(get_func get);
+
+#endif
