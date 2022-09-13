a2c149e23d10313318e4b586af216961c6206cec
Seven Lv <lvhongqiang@msn.cn>
2022-09-13 19:53:47 +0800

    add pra_get_version test

---
+++ tests/test_version.c
@@ -0,0 +1,34 @@
+#include "pra_version.h"
+#include "test.h"
+
+int main(void);
+
+int main(void)
+{
+    int result = err_none;
+
+    const uint8_t expected_major = 0U;
+    const uint8_t expected_minor = 4U;
+    const uint8_t expected_reversion = 15U;
+
+    pra_version actual_version;
+
+    actual_version = pra_get_version();
+
+    if (expected_major != actual_version.major)
+    {
+        result |= err_error1;
+    }
+
+    if (expected_minor != actual_version.minor)
+    {
+        result |= err_error2;
+    }
+
+    if (expected_reversion != actual_version.reversion)
+    {
+        result |= err_error3;
+    }
+
+    return result;
+}
